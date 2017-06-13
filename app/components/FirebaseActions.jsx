589e9c71a4c0dbe1f83add6f742fe66bb7a054c1
Ruben Ruiz <e9.ruben@gmail.com>
2017-06-13 15:00:18 -0600

    First commit, redux working with react

---
+++ app/components/FirebaseActions.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var {connect} = require('react-redux');
+var actions = require('actions');
+
+export var FirebaseActions = React.createClass({
+  render: function () {
+    var {dispatch} = this.props;
+
+    return (
+      <div className="container__header">
+        <button type="button" className="button expanded"onClick={() => {
+            dispatch(actions.toggleTodo("-KluJPZ94_CfpjtsMdOY"));
+          }}>Read</button>
+        <button type="button" className="button expanded">Write</button>
+      </div>
+    )
+  }
+});
+
+export default connect()(FirebaseActions);
